289bae99de4528cec4cd30d80f89cd0d3089d46f
Frostblazer <42157097+Frostblazer@users.noreply.github.com>
2020-06-03 18:24:16 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContinuouslyEnterANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userNumbers = new List<int>();
+            var userInput = "";
+
+            while(userInput != "Quit")
+            {
+                Console.Write("Enter a number: ");
+                userInput = Console.ReadLine();
+        
+                if(userInput != "Quit")
+                {
+                    var userNumber = Int32.Parse(userInput);
+                    userNumbers.Add(userNumber);
+                }               
+            }
+
+            var uniques = new List<int>();
+            foreach (var element in userNumbers)
+            {
+                if (!uniques.Contains(element))
+                {
+                    uniques.Add(element);
+                }                
+            }
+
+            foreach (int element in uniques)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
